6b25afc4939737fce8d433ae113effffced53a19
unknown <madhurajmadu693@gmail.com>
2023-05-07 14:19:21 +0530

    last modification login page

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 
+import os
+os.environ['FLASK_DEBUG'] = 'production'
 
-from flask import Flask, render_template, request,url_for
+from flask import Flask, render_template, request,url_for,redirect
+
 import tensorflow as tf
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 import numpy as np
 import pickle
+
 
 import re
 import nltk
@@ -26,9 +30,25 @@ model = tf.keras.models.load_model('smsspam2.h5')
 
 
 # define a route for the home page  
+
 @app.route('/')
+def index():
+    return render_template('login.html')
+@app.route('/login',methods=['POST','GET'])
+def login():
+        username=request.form['username']
+        password=request.form['password']
+
+        if username =='admin' and password =='1234':
+            return redirect(url_for('home'))
+        else:
+            error ='Invalid username or password'
+            return render_template('login.html',error=error)
+    
+    
+@app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 # load the tokenizer from a file
 with open('tokenizer.pkl', 'rb') as handle:
@@ -37,10 +57,6 @@ with open('tokenizer.pkl', 'rb') as handle:
 
 # define a route for the SMS spam detection form submission
 @app.route('/predict', methods=['POST','GET'])
-
-
-
-
 
 def predict():
     model = tf.keras.models.load_model('sms_spam.h5')
@@ -64,47 +80,26 @@ def predict():
                 cleaned_text = ' '.join(words)
                 return cleaned_text
             cleaned=cleaned_text(message)
-            
-
-                
-            # return render_template('home.html', msg=message)
-            
-            # return render_template('result.html', seq=message)
-            
-            # tokenizer_loaded.fit_on_texts(texts)
+        
             max_features=2000
             delimiter=' '
             tokenizer = Tokenizer(num_words=max_features, split=delimiter)
-            # tokenizer = Tokenizer(num_words=200,oov_token='<00V>')
+            
             tokenizer.fit_on_texts(cleaned)
-            # here message is tokenized
-            # return render_template('result3.html', tok=tokenizer)
-            # word_index=to
-            # kenizer.word_index
-
             sequences = tokenizer.texts_to_sequences(cleaned)
-            # here the message is converted into sequence of numbers
-            # return render_template('result2.html', seq=sequences)
-            # return render_template('home.html', seq=sequences)
-        
-            # maxlen = tokenizer_loaded.get('maxlen')
 
             padded = pad_sequences(sequences, maxlen=152, padding='post', truncating='post')
-            #here sequences are padded into same length
-            # return render_template('result.html', pd=padded)
-            
-            
             array=np.array(padded)
 
-            # return render_template('result2.html', pred=array)
+            
             prediction = model.predict(array)[0][0]
-            # prediction = model.predict(np.array(padded))[0][0]
+            
             if prediction >=0.5:
                 predicted=1
             else:
                 predicted=0
             
-            return render_template('home.html', pred=predicted)
+            return render_template('index.html', pred=predicted)
         else:
             return "Please provide a message to classify."
     else:
